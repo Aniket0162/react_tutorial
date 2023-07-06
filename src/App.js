d6718cd5351698b8eae8b70d50772e493b582a17
Roronoa786Zoro <aniket199mistry@gmail.com>
2023-07-06 07:22:04 +0530

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Downloaded all the requied application to run the react code like</h3>
+      <p>
+        1. Downloaded Node Module for running javaScript with out a browser.
+        2. Downloaded Vs code Editior for performing code.
+        3. Then Downloaded react dev tool. React Dev Tools is an extension created by the React team. It enables developers to debug their code inside their Developer Tools. Once you add the extension, you will get 2 new tabs in DevTools called Components and Profiler, respectively. To use the full functionality of the extension, you need to be in development mode.
+
+        4. then we open vs code Downloaded some extension that will help to perform react app fast and efficient.
+            a. Prettier.
+            b. Bracket pair colorizor.
+            c. Auto renamer .
+            d. Es7 react redux library.
+            e. Thunder Client.
+
+      </p>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
