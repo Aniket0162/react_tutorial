0ec050c3f84d4b9863c19ef680701e652d84a8a5
Roronoa786Zoro <aniket199mistry@gmail.com>
2023-07-07 08:57:25 +0530

    Day 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h2>Day 2</h2>
+      <h2>Day 3 Java Refrecer</h2>
         <p>
-          Learn how to creat react app : npx creat-react-app (file name) 
-          Learn about all the folders of react app folders & its structure.
-          learn about basic of props and state.
-          how to run react app.
+          Changes are done in index.html by creating the index file.
         </p>
       
     </div>
